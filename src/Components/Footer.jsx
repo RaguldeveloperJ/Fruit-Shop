8a2e8b7a47d05b25f9daab83c44609a17f15ccc4
RaguldeveloperJ <jkrdragul2018@gmail.com>
2025-12-23 16:51:05 +0530

    footer modified

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -9,8 +9,8 @@ const Footer = () => {
        </div>
        
 <footer class="bg-white dark:bg-gray-800 w-full py-8">
-    <div class="max-w-screen-xl px-4 mx-auto">
-        <ul class="flex flex-wrap justify-between max-w-screen-md mx-auto text-lg font-light">
+    <div class=" px-4 mx-auto">
+        <ul class="flex flex-wrap justify-between max-w-3xl mx-auto text-lg font-light">
             <li class="my-2">
                 <a class="text-gray-400 hover:text-gray-800 dark:text-gray-300 dark:hover:text-white transition-colors duration-200" href="#">
                     FAQ
@@ -67,9 +67,9 @@ const Footer = () => {
         <div class="text-center pt-10 sm:pt-12 font-light flex items-center justify-center">
             <form class="flex flex-col justify-center w-3/4 max-w-sm space-y-3 md:flex-row md:w-full md:space-x-3 md:space-y-0">
                 <div class=" relative ">
-                    <input type="text" id="&quot;form-subscribe-Subscribe" class=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Email"/>
+                    <input type="text" id="&quot;form-subscribe-Subscribe" class=" rounded-lg  flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Email"/>
                     </div>
-                    <button class="flex-shrink-0 px-4 py-2 text-base font-semibold text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200" type="submit">
+                    <button class=" px-4 py-2 text-base font-semibold text-white bg-purple-600 rounded-lg shadow-md hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2 focus:ring-offset-purple-200" type="submit">
                         Subscribe
                     </button>
                 </form>
